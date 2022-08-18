3d672eb8619243ec7819fb11643f32fa1349681a
taegi <101815874+tagtaek@users.noreply.github.com>
2022-08-18 21:21:06 +0900

    javascript: variable
    number and string

---
+++ study/javascript/variable/script.js
@@ -0,0 +1,35 @@
+let rose;
+rose = 97;
+console.log(97);
+console.log(rose);
+
+let risa;
+risa = 'blackpink'
+console.log('blackpink');
+console.log(risa);
+
+
+/*
+//변수 만드는 법 
+let 변수이름 = 데이터;
+
+// 변수의 선언과 초기화를 따로 하기 
+let 변수이름; // 변수 선언
+변수이름 = 데이터; //변수 초기화
+
+// 변수는 한번에 하나의 데이터만 기억할 수 있다. 
+let 변수이름 = 데이터1; //선언과 초기화
+변수이름 = 데이터2; //새로운 데이터 대입
+변수이름 = 데이터3; //또 새로운 데이터 대입 - 결과적으로 기억하는 데이터
+
+// 숫자 
+let number1 = 10; //정수
+let number2 = 0;
+let number3 = -20; 
+let number4 = 3.14; //실수
+let number5 = -3.14;
+
+// 문자열 
+let string1 = '문자열이다'
+let string2 = "12345...!?"
+*/
