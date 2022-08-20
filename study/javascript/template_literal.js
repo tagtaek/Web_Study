88e428057b73e8e4074445d1416a8e75d639563b
taegi <101815874+tagtaek@users.noreply.github.com>
2022-08-20 17:16:27 +0900

    JS: template literal
    practice

---
+++ study/javascript/template_literal.js
@@ -0,0 +1,13 @@
+let actor = prompt('좋아하는 외국 배우는?');
+let reason = prompt('그 이유는?');
+console.log(`내가 좋아하는 외국 배우는 ${actor} 이고, 
+그 이유는 ${reason} 하기 때문이다. `);
+/*
+const data1 = "데이터";
+const str1 = `이 문자열에 ${data1} 를 삽입할 것입니다.`;
+console.log(str1);
+
+const data2 = 97;
+const str2 = `이 문자열에 숫자 ${data2} 또한 삽입할 것입니다.`;
+console.log(str2);
+*/
