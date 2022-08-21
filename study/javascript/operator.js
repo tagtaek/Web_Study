3db948d6965d7c8b438dfdf9b0af6ba1c770247d
taegi <101815874+tagtaek@users.noreply.github.com>
2022-08-21 22:49:13 +0900

    Javascript: operator
    Basic operator

---
+++ study/javascript/operator.js
@@ -0,0 +1,14 @@
+//산술 연산자
+console.log(3+4); // 7
+console.log(3-4); // -1
+console.log(3*4); // 12
+console.log(3/4); // 0.75
+console.log(3%4); // 3
+
+let result = 1+2*3; //우선순위에 의해 나누기, 곱하기가 먼저 된다.
+console.log(result); // 7
+
+//대입 연산자
+let result = 3;
+result = result +3; // 6
+result += 3; //두 줄은 같은 의미이다.
