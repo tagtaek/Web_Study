9c68976945c5cdd1ecb013f37c472f321b2b4436
taegi <101815874+tagtaek@users.noreply.github.com>
2022-08-25 16:39:17 +0900

    Javascript: comparison operator
    abstracr & strict

---
+++ study/javascript/comparison.js
@@ -0,0 +1,14 @@
+let num1=7;
+let num2=9;
+let data = '7'
+
+console.log(num1 < num2); // true
+console.log(num1 <= num2); // true
+
+//추상적 비교: 데이터형이 달라도 모양만 같으면 됨
+//엄격한 비교: 데이터형까지 같아야 함
+console.log(num1 == data); // true
+console.log(num1 === num2); // false
+
+console.log(num1 != data); // false
+console.log(num1 !== data); // true
