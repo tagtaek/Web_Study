c77ab68ffcb6c95cb5d789d102b0a84d26bf2451
taegi <101815874+tagtaek@users.noreply.github.com>
2022-08-26 22:31:29 +0900

    Update comparison.js
    Detail..

--- study/javascript/comparison.js
+++ study/javascript/comparison.js
@@ -5,8 +5,8 @@ let data = '7'
 console.log(num1 < num2); // true
 console.log(num1 <= num2); // true
 
-//추상적 비교: 데이터형이 달라도 모양만 같으면 됨
-//엄격한 비교: 데이터형까지 같아야 함
+//추상적(abstract) 비교: 데이터형이 달라도 모양만 같으면 됨
+//엄격한(strict) 비교: 데이터형까지 같아야 함
 console.log(num1 == data); // true
 console.log(num1 === num2); // false
 
