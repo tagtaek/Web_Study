a5e2a31eab2eee6a18a53b039cd30b4e5ac783d8
taegi <101815874+tagtaek@users.noreply.github.com>
2022-08-30 23:19:10 +0900

    javascript: if~else
    if~else

---
+++ study/javascript/if-else.js
@@ -0,0 +1,14 @@
+let num = 4;
+console.log(num == 4); //true
+
+if(num == 3){
+    console.log("if문의 조건이 맞으면 이 텍스트가 출력됩니다1.")
+}else{
+    console.log("if문의 조건이 틀리면 이 텍스트가 출력됩니다1.")
+}
+
+if(num == 4){
+    console.log("if문의 조건이 맞으면 이 텍스트가 출력됩니다2.")
+}else{
+    console.log("if문의 조건이 틀리면 이 텍스트가 출력됩니다2.")
+}
